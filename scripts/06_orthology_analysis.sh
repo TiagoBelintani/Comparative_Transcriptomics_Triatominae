09c478275bac05382b2073561d447824998fe387
Tiago Belintani <113792806+TiagoBelintani@users.noreply.github.com>
2025-04-30 19:10:13 -0700

    Update 06_orthology_analysis.sh

--- scripts/06_orthology_analysis.sh
+++ scripts/06_orthology_analysis.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # OrthoFinder and OrthoVenn
-orthofinder -f ../data/assemblies/ -t 10
+orthofinder -d -M msa -S diamond -z -t 20 -I 1.5 -f *.fa -n Orthofinder
 # Upload to OrthoVenn3 web interface manually if needed
