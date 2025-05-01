e3e674a12da523ff1024a256bc10feb76b41df66
Tiago Belintani <113792806+TiagoBelintani@users.noreply.github.com>
2025-04-30 18:43:18 -0700

    Update 01_download_reads.sh

--- scripts/01_download_reads.sh
+++ scripts/01_download_reads.sh
@@ -3,6 +3,6 @@
 SAMPLES=(SRR9999991 SRR9999992 SRR9999993 SRR9999994 SRR9999995 SRR9999996)
 mkdir -p ../data/raw_reads && cd ../data/raw_reads || exit
 for SAMPLE in "${SAMPLES[@]}"; do
-    fasterq-dump --split-files "$SAMPLE" -e 4
+    fasterq-dump --gzip --defline-seq '@$sn[_$rn]/$ri' --split-files "$SAMPLE" -e 4
     gzip ${SAMPLE}_1.fastq ${SAMPLE}_2.fastq
 done
